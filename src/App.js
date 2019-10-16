1297cff362d2c06cf44769238944fdb2777ee314
Beatriz Sabbatini <beatriz.sabbatini@gmail.com>
2019-10-16 09:59:18 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+          Hello World!
+        </p>
+       
+    </div>
+  );
+}
+
+export default App;
